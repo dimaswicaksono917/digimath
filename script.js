525b59291cb48bb287a140f65112ee435b219bd5
Dimas <dimaswicaksono917@gmail.com>
2023-01-17 22:15:11 +0700

    bangun datar dekstop

---
+++ script.js
@@ -0,0 +1,13 @@
+function openMenu(evt, menuName) {
+    var i, tabcontent, sidebar;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    sidebar = document.getElementsByClassName("sidebar");
+    for (i = 0; i < sidebar.length; i++) {
+      sidebar[i].className = sidebar[i].className.replace(" active", "");
+    }
+    document.getElementById(menuName).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
