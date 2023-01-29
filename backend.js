25e8e3b867abdbb55a9ed482aa5a4598c7ca87b0
Dimas <dimaswicaksono917@gmail.com>
2023-01-29 17:40:48 +0700

    calculator

--- backend.js
+++ backend.js
@@ -88,22 +88,127 @@ function kPanjang() {
 function hitungIMT() {
     let tinggiBadan = document.getElementById("tinggiBadan").value;
     let beratBadan = document.getElementById("beratBadan").value;
-    let bmi = (beratBadan / (tinggiBadan * tinggiBadan) * 10000).toFixed(1); 
+    let bmi = (beratBadan / (tinggiBadan * tinggiBadan) * 10000).toFixed(1);
     let hasil
 
     if (bmi < 17) {
         hasil = 'Berat Badan kamu <strong>Sangat Kurus</strong>';
-    }else if (bmi >= 17.0 && bmi < 18.5) {
+    } else if (bmi >= 17.0 && bmi < 18.5) {
         hasil = 'Berat Badan kamu <strong>Kurus</strong>';
-    }else if (bmi >= 18.5 && bmi < 25.0) {
+    } else if (bmi >= 18.5 && bmi < 25.0) {
         hasil = 'Berat Badan kamu <strong>Normal</strong>';
-    }else if (bmi >= 25.0 && bmi < 27.0) {
+    } else if (bmi >= 25.0 && bmi < 27.0) {
         hasil = 'Berat Badan Kamu <strong>Gemuk</strong>';
-    }else if (bmi >= 27.0) {
+    } else if (bmi >= 27.0) {
         hasil = 'Berat Badan Kamu <strong>Obesitas</strong>';
     }
     console.log(bmi)
 
-   document.getElementById("hasilIMT").innerHTML = hasil;
-   document.getElementById("ankaIMT").innerHTML = bmi;
+    document.getElementById("hasilIMT").innerHTML = hasil;
+    document.getElementById("ankaIMT").innerHTML = bmi;
 }
+
+// calculator
+const calculator = {
+    displayValue: '0',
+    firstOperand: null,
+    waitingForSecondOperand: false,
+    operator: null,
+  };
+  
+  function inputDigit(digit) {
+    const { displayValue, waitingForSecondOperand } = calculator;
+  
+    if (waitingForSecondOperand === true) {
+      calculator.displayValue = digit;
+      calculator.waitingForSecondOperand = false;
+    } else {
+      calculator.displayValue = displayValue === '0' ? digit : displayValue + digit;
+    }
+  }
+  
+  function inputDecimal(dot) {
+    // If the `displayValue` does not contain a decimal point
+    if (!calculator.displayValue.includes(dot)) {
+      // Append the decimal point
+      calculator.displayValue += dot;
+    }
+  }
+  
+  function handleOperator(nextOperator) {
+    const { firstOperand, displayValue, operator } = calculator
+    const inputValue = parseFloat(displayValue);
+  
+    if (operator && calculator.waitingForSecondOperand)  {
+      calculator.operator = nextOperator;
+      return;
+    }
+  
+    if (firstOperand == null) {
+      calculator.firstOperand = inputValue;
+    } else if (operator) {
+      const currentValue = firstOperand || 0;
+      const result = performCalculation[operator](currentValue, inputValue);
+  
+      calculator.displayValue = String(result);
+      calculator.firstOperand = result;
+    }
+  
+    calculator.waitingForSecondOperand = true;
+    calculator.operator = nextOperator;
+  }
+  
+  const performCalculation = {
+    '/': (firstOperand, secondOperand) => firstOperand / secondOperand,
+  
+    '*': (firstOperand, secondOperand) => firstOperand * secondOperand,
+  
+    '+': (firstOperand, secondOperand) => firstOperand + secondOperand,
+  
+    '-': (firstOperand, secondOperand) => firstOperand - secondOperand,
+  
+    '=': (firstOperand, secondOperand) => secondOperand
+  };
+  
+  function resetCalculator() {
+    calculator.displayValue = '0';
+    calculator.firstOperand = null;
+    calculator.waitingForSecondOperand = false;
+    calculator.operator = null;
+  }
+  
+  function updateDisplay() {
+    const display = document.querySelector('.calculator-screen');
+    display.value = calculator.displayValue;
+  }
+  
+  updateDisplay();
+  
+  const keys = document.querySelector('.calculator-keys');
+  keys.addEventListener('click', (event) => {
+    const { target } = event;
+    if (!target.matches('button')) {
+      return;
+    }
+  
+    if (target.classList.contains('operator')) {
+      handleOperator(target.value);
+      updateDisplay();
+      return;
+    }
+  
+    if (target.classList.contains('decimal')) {
+      inputDecimal(target.value);
+      updateDisplay();
+      return;
+    }
+  
+    if (target.classList.contains('all-clear')) {
+      resetCalculator();
+      updateDisplay();
+      return;
+    }
+  
+    inputDigit(target.value);
+    updateDisplay();
+  });
